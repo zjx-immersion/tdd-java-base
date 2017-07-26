5eda102988b041da1dc901d3d0ce719bd4a7361b
zjx-immersion <jianxin_home@163.com>
2017-07-26 15:46:18 +0800

    [Jason]Added the file read and main entre

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,14 +11,21 @@ import static java.util.stream.Collectors.joining;
  */
 public class WordFrequencyGame {
     public String process(String inputStr) {
-        String[] wordArr = inputStr.split("\\s+");
-        List<Word> wordList = Arrays.stream(wordArr).map(w -> new Word(w, 1)).collect(Collectors.toList());
+        String[] wordArr = inputStr.split("");
+
+        List<Word> wordList = Arrays.stream(wordArr)
+                .map(w -> new Word(w, 1)).collect(Collectors.toList());
+
         wordList = wordList.stream()
                 .collect(groupingBy(w -> w.getValue()))
                 .entrySet().stream()
                 .map(entry -> new Word(entry.getKey(), entry.getValue().size()))
                 .collect(Collectors.toList());
+
         wordList.sort((w1, w2) -> w2.getCount() - w1.getCount());
-        return wordList.stream().map(w -> w.getValue() + " " + w.getCount()).collect(joining("\n"));
+
+        return wordList.stream()
+                .map(w -> w.getValue() + " " + w.getCount())
+                .collect(joining("\n"));
     }
 }
--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -1,27 +1,20 @@
 import org.junit.Test;
 
-import java.util.stream.Stream;
-
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by jxzhong on 2017/7/25.
  */
 public class WordFrequencyGameTest {
+
+
+
     @Test
     public void should_get_the_1_when_input_the() throws Exception {
         //Given
         String inputStr = "the";
         String expectResult = "the 1";
         validate_Input_words_process_to_expected_word(inputStr, expectResult);
-    }
-
-    private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) {
-        WordFrequencyGame game = new WordFrequencyGame();
-        //When
-        String result = game.process(inputStr);
-        //Then
-        assertEquals(result, expectResult);
     }
 
     @Test
@@ -35,7 +28,7 @@ public class WordFrequencyGameTest {
     @Test
     public void should_process_two_words_with_special_spaces() throws Exception {
         //Given
-        String inputStr = "the       is";
+        String inputStr = "the      is";
         String expectResult = "the 1\nis 1";
         validate_Input_words_process_to_expected_word(inputStr, expectResult);
     }
@@ -62,5 +55,13 @@ public class WordFrequencyGameTest {
         String inputStr = "the is is";
         String expectResult = "is 2\nthe 1";
         validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    }
+
+    private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) {
+        WordFrequencyGame game = new WordFrequencyGame();
+        //When
+        String result = game.process(inputStr);
+        //Then
+        assertEquals(result, expectResult);
     }
 }
