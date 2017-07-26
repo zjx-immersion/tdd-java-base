797f1b5cafabb69df190050723d554b34206ffa4
zjx-immersion <jianxin_home@163.com>
2017-07-26 13:02:30 +0800

    [Jason]add 3 test cases and implementations

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 
 /**
@@ -12,6 +13,12 @@ public class WordFrequencyGame {
     public String process(String inputStr) {
         String[] wordArr = inputStr.split(" ");
         List<Word> wordList = Arrays.stream(wordArr).map(w -> new Word(w, 1)).collect(Collectors.toList());
-        return wordList.stream().map( w -> w.getValue() + " " + w.getCount()).collect(joining("/n"));
+        wordList = wordList.stream()
+                .collect(groupingBy(w -> w.getValue()))
+                .entrySet().stream()
+                .map(entry -> new Word(entry.getKey(), entry.getValue().size()))
+                .collect(Collectors.toList());
+        wordList.sort((w1, w2) -> w2.getCount() - w1.getCount());
+        return wordList.stream().map(w -> w.getValue() + " " + w.getCount()).collect(joining("/n"));
     }
 }
--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -12,22 +12,39 @@ public class WordFrequencyGameTest {
     public void should_get_the_1_when_input_the() throws Exception {
         //Given
         String inputStr = "the";
+        String expectResult = "the 1";
+        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    }
+
+    private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) {
         WordFrequencyGame game = new WordFrequencyGame();
         //When
         String result = game.process(inputStr);
         //Then
-        assertEquals(result, "the 1");
+        assertEquals(result, expectResult);
     }
 
     @Test
     public void should_process_two_words() throws Exception {
         //Given
         String inputStr = "the is";
-        WordFrequencyGame game = new WordFrequencyGame();
-        //When
-        String result = game.process(inputStr);
+        String expectResult = "the 1/nis 1";
+        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    }
 
-        //Then
-        assertEquals(result, "the 1/nis 1");
+    @Test
+    public void should_pracess_two_same_words_with_sorted() throws Exception {
+        //Given
+        String inputStr = "the the is";
+        String expectResult = "the 2/nis 1";
+        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    }
+
+    @Test
+    public void should_process_sorted_with_count_descending() throws Exception {
+        //Given
+        String inputStr = "the is is";
+        String expectResult = "is 2/nthe 1";
+        validate_Input_words_process_to_expected_word(inputStr, expectResult);
     }
 }
